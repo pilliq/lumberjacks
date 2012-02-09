fb27c41854e3d45a3b7abf84b8271130e8bacc91
Greg Job <gregjob@ubuntu.(none)>
2012-02-08 21:10:36 -0500

    Greg's Initial Start To assignment

--- shell.c
+++ shell.c
@@ -1,44 +1,102 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include "shell.h"
+#include <unistd.h>
+#define DELIMINATORS "\t "
 
-int
-main(int argc, char *argv[])
+
+/*************** (BUILT IN FUNCTIONS) **************************/
+int CD (char* command)
 {
-  char line[50*10];
-  char **tokens;
-  int i;
+	return chdir(command);
+}
 
-  while ((tokens = calloc(11, sizeof(char*))) != NULL);
-
-  for (i = 0; i < sizeof(tokens); i++)
-  {
-    printf("%d\n", i);
-  }
-
-  while (1)
-  {
-    fgets(line, sizeof(line), stdin);
-    printf("%s", line);
-    if (strcmp(line, "exit") == 0)
+char* parseCommandLine(char* arguments)
+{
+	int size = 0;
+	/* assume there are 256 tokens */
+	char* commandLine = (char*) malloc (257 * sizeof (char));
+	commandLine[256] = '\0';
+	
+	char* token = NULL;
+    token = strtok(arguments, "| ");
+    
+	while (token != NULL)
     {
-      printf("Hi\n");
-      break;
-    }
-  }
+		commandLine[size] = token;
+		size++;          
+        token = strtok(NULL, "| ");
+    } 
+	return commandLine;	
 }
 
-/**
- * Takes a list of token and returns a list of commands.
- * @return array of strings
- */
-char***
-get_command_list(char* tokens[])
+Dictionary buildFunctionDictionary()
 {
-  int i;
-  for (i = 0; i < sizeof(tokens); i++)
-  {
-    printf("i");
-  }
-  return 0;
+		
+Function createFunction(char* command)
+{
+	if (strcmp(command, "cd") == 0)
+	{
+		Function function = malloc (sizeof (struct funct));
+		function -> command = strdup(command);
+		function -> f = CD; 	
+	}	
 }
+int main(int argc, char **argv)
+{
+	int i;
+	char* arguments = NULL;
+	
+	arguments = (char*) malloc (257 * sizeof (char));
+	/* some code taken here: http://www.daniweb.com/software-development/c/tutorials/45806 */
+	fputs("$ ", stdout);
+	fflush(stdout);
+	fgets(arguments, 257, stdin);
+	/*printf("%s\n", arguments);
+	printf("%d\n", strlen(arguments));
+	*/
+	
+	/* fix bug where when you press enter after typing command, you get a newline character
+	at the end of your string. Add terminator character to the end */ 
+	if (arguments[ (strlen(arguments) -1)] == '\n')
+	{
+		arguments[ (strlen(arguments) -1)] = '\0';
+		
+	}
+	
+	/* figure out way to handle tokens within single / double quotes */
+	
+	while (arguments != NULL && strcmp(arguments, "quit") != 0)
+	{	
+		char* token = NULL;
+		token = strtok(arguments, DELIMINATORS);
+		while (token != NULL)
+		{	
+			/* printf("%d\n", token[0]);
+			printf("%d\n", token[strlen(token)-1]); */
+			printf("%s\n", token );
+			if (strcmp(token, "|") == 0)
+			{
+				printf("end of command. create new command\n");
+			}
+			//free(token);			
+			token = strtok(NULL, DELIMINATORS);
+		}	
+		free(arguments);
+		arguments = (char*) malloc (257 * sizeof (char));
+		fputs("$ ", stdout);
+        fflush(stdout);
+        fgets(arguments, 257, stdin);
+		/* printf("%d:%s\n", strlen(arguments), arguments); */
+        
+		if (arguments[ (strlen(arguments) -1)] == '\n')
+        {
+			arguments[ (strlen(arguments) -1)] = '\0';
+	    }
+	
+		//printf("Freed data\n");
+	}
+
+	return 1;
+} 
