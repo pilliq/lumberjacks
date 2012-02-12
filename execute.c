58f319b85e2dab1ced82ec8171f9fcbc8b82ca90
Gordon Arnold <goarnold908@gmail.com>
2012-02-12 15:37:43 -0500

    execute

--- execute.c
+++ execute.c
@@ -24,3 +24,27 @@ else if(strcmp(command,"cd")==1||strcmp(command,"exit")==1)
 }
 return 0;
 }
+
+int fork()
+{	
+	void runit(void);
+	int pid;	/* process ID */
+	switch (pid = fork()) {
+	case 0:		/* a fork returns 0 to the child */
+		runit();
+		break;
+	default:	/* a fork returns a pid to the parent */	
+		sleep(5);
+		printf("I'm still here!\n");
+		break;										case -1:	/* something went wrong */							perror("fork");										exit(1);
+	}
+	exit(0);
+}
+
+void runit(void) {
+	printf("About to run ls\n";
+	exelp("ls","ls", "-aF", "/" , (char*)0);
+	perror("execlp");
+	exit(1);
+}
+	
