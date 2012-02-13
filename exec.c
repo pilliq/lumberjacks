a3867a52f6eba6514f1739f37fd65e1123646d3b
Phillip Quiza <aznwhtey@gmail.com>
2012-02-12 21:34:03 -0500

    execution

---
+++ exec.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+
+#include "util.h"
+
+void exec(Commands c) {
+    commandNode ptr;
+
+    while (ptr = c->head) {
+        char* argptr; 
+        while (argptr =
+        printf("Command: %s\n", ptr->command);
+    }
+}
---
+++ exec.h
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+
+#include "util.h"
+
+void exec(Commands c);
